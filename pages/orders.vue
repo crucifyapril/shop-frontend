d6f928b311a86c69ae6ddde5886a5f49b4354fc5
crucifyapril <sayaf3@yahoo.com>
2025-01-13 21:12:27 +0300

    #4 интергрировано апи orders

--- pages/orders.vue
+++ pages/orders.vue
@@ -1,7 +1,47 @@
 <script setup lang="ts">
-
 import HeaderComponent from "~/components/Header.vue";
 import FooterComponent from "~/components/Footer.vue";
+import { useRuntimeConfig } from "#app";
+import type { ApiResponse, Order } from "~/types/OrderTypes";
+import {useAuthStore} from '~/stores/auth';
+import {computed} from 'vue';
+
+const authStore = useAuthStore();
+
+const userName = computed(() => authStore.user?.name || 'Гость');
+
+const config = useRuntimeConfig();
+const baseURL = config.public.apiBase;
+
+const page = ref(1);
+const orders = ref<Order[]>([]);
+const totalPages = ref(0);
+const isLoading = ref(false);
+
+const fetchOrders = async () => {
+  try {
+    isLoading.value = true;
+    const { data } = await useFetch<ApiResponse>(`${baseURL}/api/orders?page=${page.value}`);
+    if (data.value) {
+      orders.value = data.value.data;
+      totalPages.value = data.value.last_page;
+    }
+  } catch (error) {
+    console.error("Ошибка загрузки данных:", error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+await fetchOrders();
+
+const goToPage = async (newPage: number) => {
+  if (newPage > 0 && newPage <= totalPages.value) {
+    page.value = newPage;
+    await fetchOrders();
+  }
+};
+
 </script>
 
 <template>
@@ -12,6 +52,9 @@ import FooterComponent from "~/components/Footer.vue";
     <main class="flex-grow container mx-auto py-8">
       <h1 class="flex items-center justify-center text-3xl font-bold mt-10 mb-16">Мои заказы</h1>
 
+      <div v-if="isLoading" class="flex justify-center items-center">
+        <span>Загрузка...</span>
+      </div>
 
 
       <div class="relative overflow-x-auto m-auto w-2/3">
@@ -39,21 +82,22 @@ import FooterComponent from "~/components/Footer.vue";
           </tr>
           </thead>
           <tbody>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <tr
+              v-for="order in orders" :key="order.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              order id
+              {{ order.id }}
             </th>
             <td class="px-6 py-4 text-center">
-              user.name | user.email
+              {{ userName }}
             </td>
             <td class="px-6 py-4 text-center">
-              status.name
+              {{ order.status_id === 1 ? 'Pending' : 'unknown status' }}
             </td>
             <td class="px-6 py-4 text-center">
-              total_amount руб
+              {{ order.total_amount }} руб
             </td>
             <td class="px-6 py-4 text-center">
-              created_at
+              {{ order.created_at }}
             </td>
             <td class="px-6 py-4 text-center">
               <a href="#">
@@ -66,6 +110,26 @@ import FooterComponent from "~/components/Footer.vue";
           </tbody>
         </table>
       </div>
+
+        <div class="flex items-center justify-center m-auto mt-8 w-1/4">
+          <button
+              @click="goToPage(page - 1)"
+              :disabled="page === 1"
+              class="flex items-center justify-center px-4 h-10 w-1/2 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg
+              hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white disabled:pointer-events-none disabled:opacity-50"
+          >
+            Previous
+          </button>
+
+          <button
+              @click="goToPage(page + 1)"
+              :disabled="page === totalPages"
+              class="flex items-center justify-center px-4 h-10 w-1/2 ms-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg
+              hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white disabled:pointer-events-none disabled:opacity-50"
+          >
+            Next
+          </button>
+        </div>
     </main>
 
 
