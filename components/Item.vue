c3eb52fcd3097bdfe357087f4e9e33bff7c2440b
crucifyapril <sayaf3@yahoo.com>
2025-01-13 21:19:11 +0300

    #4 подзаказ и заказ теперь не вместе

--- components/Item.vue
+++ components/Item.vue
@@ -23,20 +23,24 @@ defineProps<{
       </a>
       <div class="flex items-center justify-between">
         <span class="text-3xl font-bold text-gray-900 dark:text-white">{{ product.price }} руб.</span>
-        <form action="#">
-          <button type="submit"
-                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
+        <div v-if="product.quantity === 0">
+          <form action="#">
+            <button type="submit"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
                   dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Под заказ
-          </button>
-        </form>
-        <form action="#" method="POST">
-          <input type="hidden" name="product_id" :value="product.id">
-          <input type="hidden" name="quantity" value="1">
-          <button type="submit"
-                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
+            </button>
+          </form>
+        </div>
+        <div v-else>
+          <form action="#" method="POST">
+            <input type="hidden" name="product_id" :value="product.id">
+            <input type="hidden" name="quantity" value="1">
+            <button type="submit"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
                   dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">В корзину
-          </button>
-        </form>
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   </div>
